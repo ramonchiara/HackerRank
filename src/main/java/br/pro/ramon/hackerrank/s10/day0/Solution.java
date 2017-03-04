783ba9c47ad16f62fe2d1446b94d44e39cdd3806
Ramon Chiara <ramonchiara@gmail.com>
2017-03-03 21:23:14 -0300

    10 Days of Statistics - Day 0: Mean, Median, and Mode

---
+++ src/main/java/br/pro/ramon/hackerrank/s10/day0/Solution.java
@@ -0,0 +1,49 @@
+package br.pro.ramon.hackerrank.s10.day0;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int[] v = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            v[i] = scan.nextInt();
+        }
+
+        System.out.printf("%.1f%n%.1f%n%d%n", getMean(v), getMedian(v), getMode(v));
+    }
+
+    public static double getMean(int[] v) {
+        return Arrays.stream(v)
+                .average()
+                .getAsDouble();
+    }
+
+    public static double getMedian(int[] v) {
+        return Arrays.stream(v)
+                .sorted()
+                .skip(v.length / 2 - 1)
+                .limit(v.length % 2 == 0 ? 2 : 1)
+                .average()
+                .getAsDouble();
+    }
+
+    public static int getMode(int[] v) {
+        return Arrays.stream(v)
+                .mapToObj(Integer::new)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .findFirst()
+                .get()
+                .getKey();
+    }
+
+}
