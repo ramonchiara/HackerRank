00f2b28d9a0f0944d1849fb88e6413cfc9bb67ca
Ramon Chiara <ramonchiara@gmail.com>
2021-05-19 14:29:32 -0300

    10 Days of Statistics - Day 4: Geometric Distribution I

---
+++ src/main/java/br/pro/ramon/hackerrank/s10/day4/geometric1/Solution.java
@@ -0,0 +1,42 @@
+package br.pro.ramon.hackerrank.s10.day4.geometric1;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+        int den = in.nextInt();
+        int n = in.nextInt();
+
+        double p = (double) num / den;
+
+        double result = g(n, p);
+
+        System.out.printf("%.3f%n", result);
+    }
+
+    public static double g(double n, double p) {
+        double q = 1 - p;
+        return Math.pow(q, n - 1) * p;
+    }
+
+    public static double bStar(int x, int n, double p) {
+        double q = 1 - p;
+        return c(n - 1, x - 1) * Math.pow(p, x) * Math.pow(q, n - x);
+    }
+
+    public static double c(int n, int x) {
+        return (double) fat(n) / (fat(x) * fat(n - x));
+    }
+
+    public static double fat(int n) {
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f *= i;
+        }
+        return f;
+    }
+}
